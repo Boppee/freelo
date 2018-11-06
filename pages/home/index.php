4b0467965c6ff9135a0b3993d3beba600ce84051
Boppee <44766060+Boppee@users.noreply.github.com>
2018-11-06 12:58:56 +0100

    Footer

--- pages/home/index.php
+++ pages/home/index.php
@@ -2,7 +2,7 @@
   require 'pages/inc/header.php';
   ?>
     <div class="inner">
-      test
+      
     </div>
   <?php
   require 'pages/inc/footer.php';
---
+++ pages/inc/footer.php
@@ -0,0 +1,24 @@
+<footer>
+  <div class="ad">
+
+  </div>
+  <div class="inner">
+    <div id="upperfoot">
+      <div id="socials">
+
+      </div>
+      <div id="support">
+
+      </div>
+    </div>
+    <div id="lowerfoot">
+      <div id="copytright">
+        Copyright © 2018-<?php echo date("Y"); ?> <a href="http://mrboppe.se/freelo">Emil Sandberg</a> All Rights Reserved.
+      </div>
+      <div id="juridisk">
+        <a href="#">Terms of use</a>
+        <a href="#">Licenses</a>
+      </div>
+    </div>
+  </div>
+</footer>
