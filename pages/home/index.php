abc3fdb62102d99e123b3bb4d2b7bc76a8cfd658
Boppee <44766060+Boppee@users.noreply.github.com>
2018-11-07 09:49:35 +0100

    footer / css reset

--- pages/home/index.php
+++ pages/home/index.php
@@ -2,8 +2,8 @@
   require 'pages/inc/header.php';
   ?>
     <div class="inner">
-      
+
     </div>
   <?php
-  require 'pages/inc/footer.php';
+  require 'pages/inc/footer/footer.php';
 ?>
---
+++ pages/inc/footer/footer.php
@@ -0,0 +1,52 @@
+<footer>
+  <div class="ad" id="adbannerfoot">
+
+  </div>
+  <div class="inner">
+    <div id="upperfoot">
+      <div id="socials">
+
+        <div id="instagram" class="social">
+          <div class="simg">
+            <a href="#"><img src="img/socials/instagram.png" alt=""></a>
+          </div>
+          <div class="sname">
+            @freelo
+          </div>
+        </div>
+
+        <div id="facebook" class="social">
+          <div class="simg">
+            <a href="#"><img src="img/socials/fb.png" alt=""></a>
+          </div>
+          <div class="sname">
+            @freelo
+          </div>
+        </div>
+
+        <div id="twitter" class="social">
+          <div class="simg">
+            <a href="#"><img src="img/socials/twitter.png" alt=""></a>
+          </div>
+          <div class="sname">
+            @freelo
+          </div>
+        </div>
+
+      </div>
+      <div id="support">
+
+      </div>
+    </div>
+    <div id="lowerfoot">
+      <div id="copytright">
+        Copyright © 2018-<?php echo date("Y"); ?> <a href="http://mrboppe.se/freelo">Emil Sandberg</a> All Rights Reserved.
+      </div>
+      <div id="juridisk">
+        <a href="#">Terms of use</a>
+        <a href="#">Licenses</a>
+      </div>
+    </div>
+  </div>
+</footer>
+<link rel="stylesheet" href="css/pages/inc/footer.css">
